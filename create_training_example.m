960b2202442017e8896ed9cbe7942d892b590af1
sunnymilk- <orb33@yahoo.com>
2014-12-03 15:16:31 -0500

    minor

--- create_training_example.m
+++ create_training_example.m
@@ -41,8 +41,11 @@ function create_training_example(cube_number, object_number)
     
     seg = omniwebcube2im(seg);
     
-    
     us_loc = find(vol_fn, '_');
+    
+    disp(us_loc);
+    disp(find(vol_fn, '.', 'first'));
+    
     us_loc(end+1) = find(vol_fn, '.', 'first');
     
     vol_coords = [str2double(vol_fn(us_loc(1)+2:us_loc(2)-1)), ...
