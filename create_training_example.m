14c34a2d384aa5694e56556193c850708d1c16b6
sunnymilk- <orb33@yahoo.com>
2014-12-04 18:27:18 -0500

    debug

--- create_training_example.m
+++ create_training_example.m
@@ -125,6 +125,8 @@ function create_training_example(cube_number, object_number)
         end
     end
     
+    disp(size(edge_mat));
+    
     edge_mat(:,:,1) = edge_mat(:,:,1) + edge_mat(:,:,1)';
     edge_mat(:,:,2) = min(cat(3, edge_mat(:,:,2), edge_mat(:,:,2)'),3);
     edge_mat(:,:,3) = max(cat(3, edge_mat(:,:,3), edge_mat(:,:,3)'),3);
