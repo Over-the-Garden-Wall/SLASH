fb6f925972ad35d86715f616ed7b9a45be780277
sunnymilk- <orb33@yahoo.com>
2015-01-22 22:31:55 -0500

    minor bug
    triu(X,1) to remove self edges

--- create_training_example.m
+++ create_training_example.m
@@ -155,7 +155,7 @@ function create_training_example(cube_number, object_number)
     
     
     is_good_edge = (edge_mat(:,:,4)>0) & (seg_is_in *ones(1,length(seg_is_in)) | ones(length(seg_is_in),1)*seg_is_in');
-    is_good_edge = triu(is_good_edge);
+    is_good_edge = triu(is_good_edge, 1);
     good_edge_list = find(is_good_edge(:));
     [xs ys] = ind2sub(size(is_good_edge), good_edge_list);
 
